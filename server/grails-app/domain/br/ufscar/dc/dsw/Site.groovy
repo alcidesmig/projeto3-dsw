b0a353ef1cbdf50e8ffa4d4cfa934be62b96fe2f
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-07-01 08:33:37 -0300

    Modelando classes: site de venda

--- server/grails-app/domain/br/ufscar/dc/dsw/Site.groovy
+++ server/grails-app/domain/br/ufscar/dc/dsw/Site.groovy
@@ -3,7 +3,18 @@ package br.ufscar.dc.dsw
 
 import grails.rest.*
 
-@Resource(readOnly = false, formats = ['json', 'xml'])
+@Resource(uri = '/site', readOnly = false, formats = ['json', 'xml'])
 class Site {
-
+    static constraints = {
+         email blank: false 
+         senha blank: false
+         url blank: false
+         nome blank: false
+         telefone blank: false
+    }
+    String email;
+    String senha;
+    String url;
+    String nome;
+    String telefone;
 }
