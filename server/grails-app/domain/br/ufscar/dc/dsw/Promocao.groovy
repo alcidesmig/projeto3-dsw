938b0164915473b413ca6d405e6771c4c68162a2
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-07-01 08:31:49 -0300

    Modelando classes: promoção

--- server/grails-app/domain/br/ufscar/dc/dsw/Promocao.groovy
+++ server/grails-app/domain/br/ufscar/dc/dsw/Promocao.groovy
@@ -3,7 +3,19 @@ package br.ufscar.dc.dsw
 
 import grails.rest.*
 
-@Resource(readOnly = false, formats = ['json', 'xml'])
+@Resource(uri='/promocao', readOnly = false, formats = ['json', 'xml'])
 class Promocao {
-
+    static constraints = {
+         nome blank: false 
+         site nullable: false
+         teatro nullable: false
+         preco blank: false
+         dia_hora blank: false
+         preco min: 0.00
+    }
+    Site site;
+    Teatro teatro;
+    String nome;
+    Double preco;
+    Date dia_hora;
 }
