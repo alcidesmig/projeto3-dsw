4aa36238e941bc4b099aae109a3e8aa9e2085879
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-07-03 21:55:58 -0300

    Retirando constraints de promoção

--- server/grails-app/domain/br/ufscar/dc/dsw/Promocao.groovy
+++ server/grails-app/domain/br/ufscar/dc/dsw/Promocao.groovy
@@ -5,14 +5,6 @@ import grails.rest.*
 
 @Resource(uri='/promocao', readOnly = false, formats = ['json', 'xml'])
 class Promocao {
-    static constraints = {
-         nome blank: false 
-         site nullable: false
-         teatro nullable: false
-         preco blank: false
-         dia_hora blank: false
-         preco min: 0.00
-    }
     int id;
     Site site;
     Teatro teatro;
